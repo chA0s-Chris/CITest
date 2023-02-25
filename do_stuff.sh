eb3127b991976a1882a89638d6eb1e21b37b52ab
Christian Flessa <chris@z0rn.net>
2023-02-25 21:56:54 +0100

    wip

--- do_stuff.sh
+++ do_stuff.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 #
 
-whoami
-docker ps
-echo ${CI_DOCKER_TOKEN} | docker login -u ${CI_DOCKER_LOGIN} --password-stdin
+DOCKER_PLUGIN_PATH="~/.docker/cli-plugins"
+DOCKER_PUSHRM_CMD="${DOCKER_PLUGIN_PATH}/docker-pushrm"
+DOCKER_PUSHRM_RELEASE="https://github.com/christian-korneck/docker-pushrm/releases/download/v1.9.0/docker-pushrm_linux_amd64"
+
+mkdir -p ${DOCKER_PLUGIN_PATH}
+curl -L ${PUSHRM_RELEASE} -o ${DOCKER_PUSHRM_CMD}
+chmod +x ${DOCKER_PUSHRM_CMD}
+
+docker pushrm 
+
+
+
+
+
+#echo ${CI_DOCKER_TOKEN} | docker login -u ${CI_DOCKER_LOGIN} --password-stdin
